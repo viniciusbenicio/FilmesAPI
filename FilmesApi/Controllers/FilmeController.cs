40c7504c18a17894018586a9eec2d7297cea7e62
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-02-28 19:57:33 -0300

    Criando retorno de todos Filmes adicionados

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -7,21 +7,25 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    //Lista de Filmes 
+    //Lista de Filmes...
     private static List<Filme> filmes = new List<Filme>();
 
-    //Criando um Filme..
+    //Criando um Filme...
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
-        
             filmes.Add(filme);
             Console.WriteLine
-                         ($"  Titulo  : {filme.Titulo}, " +
-                          $"  Genero  : {filme.Genero}, " +
+                         ($"  Titulo  : {filme.Titulo} " +
+                          $"  Genero  : {filme.Genero} " +
                           $"  Duração : {filme.Duracao} ");
-       
-        
+    }
+
+    //Recuperando os Filmes...
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return filmes;
     }
 
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -10,7 +10,6 @@ public class Filme
     [MaxLength(50, ErrorMessage = "O Tamanho do Genero não pode execder 50 caracteres")]
     public string Genero { get; set; }
     [Required()]
-    [Range(70, 600, ErrorMessage = "A Duração deve ser entre 70 e 600 minutos")]
+    [Range(70, 600, ErrorMessage = "A Duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
-
 }
