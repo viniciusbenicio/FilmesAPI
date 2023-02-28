3fbd5ab82ba28e2628aeb7bd597a3e0695c52550
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-02-28 19:42:35 -0300

    Criando FilmeController e o Metodo Post Para Adicionar um Filme

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    //Lista de Filmes 
+    private static List<Filme> filmes = new List<Filme>();
+
+    //Criando um Filme..
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        
+            filmes.Add(filme);
+            Console.WriteLine
+                         ($"  Titulo  : {filme.Titulo}, " +
+                          $"  Genero  : {filme.Genero}, " +
+                          $"  Duração : {filme.Duracao} ");
+       
+        
+    }
+
+}
