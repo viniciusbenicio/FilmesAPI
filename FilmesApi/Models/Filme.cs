4ca9f190a0ff86039d5374fb49fc3736bb3197c9
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-02-28 19:50:04 -0300

    Criando validação do metodo Adicionar Filme utilizando Required e ErrorMessage para validação

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,9 +1,16 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O Título do filme é obrigatório")]
     public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Genero do titulo é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O Tamanho do Genero não pode execder 50 caracteres")]
     public string Genero { get; set; }
+    [Required()]
+    [Range(70, 600, ErrorMessage = "A Duração deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 
 }
