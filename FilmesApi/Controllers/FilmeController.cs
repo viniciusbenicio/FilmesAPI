4224d80f3064a359ee2a9c5e5b6a3d51027e9f62
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-01 18:50:30 -0300

    Realizando operações para salvar no banco de Dados SQL SERVER

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -7,16 +8,20 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    //Lista de Filmes...
-    private static List<Filme> filmes = new List<Filme>();
-    private static int Id = 0;
+
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     //Criando um Filme...
     [HttpPost]
     public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
-            filme.Id = Id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmePorId), 
                 new {id = filme.Id}, 
                           filme);
@@ -26,14 +31,14 @@ public class FilmeController : ControllerBase
     [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 50)
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     //Recuperando Filme por Id...
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
         if(filme == null) return NotFound();
 
--- FilmesApi/Data/FilmeContext.cs
+++ FilmesApi/Data/FilmeContext.cs
@@ -1,16 +1,17 @@
 ﻿using FilmesApi.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace FilmesApi.Data;
-
-public class FilmeContext : DbContext
+namespace FilmesApi.Data
 {
-	public FilmeContext(DbContextOptions<FilmeContext> opts)
-		: base(opts)	
-	{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+            : base(opts)
+        {
 
-	}
+        }
 
-	public DbSet<Filme> Filmes  { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
 
+    }
 }
