f355d638ff9f0b6255fda0d77c022c035c229641
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-11 19:53:04 -0300

    Adicionando o Endereco

---
+++ FilmesApi/Profiles/EnderecoProfiler.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class EnderecoProfiler : Profile
+{
+    public EnderecoProfiler()
+    {
+        CreateMap<CreateEnderecoDto, Endereco>();
+        CreateMap<UpdateEnderecoDto, Endereco>();
+        CreateMap<Endereco, UpdateEnderecoDto>();
+        CreateMap<Endereco, ReadEnderecoDto>();
+    }
+}
