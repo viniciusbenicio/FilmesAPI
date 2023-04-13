b2ad8f7a72bef7b832c9714b8c74ca5f95701398
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-13 20:30:18 -0300

    Relacionando Endereço e Cinema

--- FilmesApi/Data/Dtos/CreateCinemaDto.cs
+++ FilmesApi/Data/Dtos/CreateCinemaDto.cs
@@ -6,4 +6,5 @@ public class CreateCinemaDto
 {
     [Required(ErrorMessage = "O Campo de Nome é obrigatório.")]
     public string Nome { get; set; }
+    public int EnderecoId { get; set; }
 }
--- FilmesApi/Models/Cinema.cs
+++ FilmesApi/Models/Cinema.cs
@@ -9,4 +9,7 @@ public class Cinema
     public int Id { get; set; }
     [Required(ErrorMessage = "O Campo de Nome é obrigatório.")]
     public string Nome { get; set; }
+    public int EnderecoId { get; set; }
+    public virtual Endereco Endereco { get; set; }
+
 }
