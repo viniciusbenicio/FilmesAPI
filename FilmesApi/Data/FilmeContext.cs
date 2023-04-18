b3b6a60e97e5e3689aab3f099bb09661622dc0fc
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-04-17 21:53:46 -0300

    Tipos de deleção

--- FilmesApi/Data/FilmeContext.cs
+++ FilmesApi/Data/FilmeContext.cs
@@ -23,6 +23,10 @@ namespace FilmesApi.Data
                         .HasOne(sessao => sessao.Filme)
                         .WithMany(filme => filme.Sessoes)
                         .HasForeignKey(sessao => sessao.FilmeId);
+            modelBuilder.Entity<Endereco>()
+                        .HasOne(endereco => endereco.Cinema)
+                        .WithOne(cinema => cinema.Endereco)
+                        .OnDelete(DeleteBehavior.Restrict);
         }
 
         public DbSet<Filme> Filmes { get; set; }
